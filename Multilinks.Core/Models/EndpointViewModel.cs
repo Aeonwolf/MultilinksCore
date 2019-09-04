62aeddb864474713f373a9d918da31dde03aa700
Chris Dinh <dinh.thanhtoan@gmail.com>
2019-09-04 21:22:54 +1200

    gh-mc-3: Renamed files and folders

---
+++ Multilinks.Core/Models/EndpointViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Multilinks.ApiService.Infrastructure;
+using Newtonsoft.Json;
+using System;
+
+namespace Multilinks.ApiService.Models
+{
+   public class EndpointViewModel : Resource, IEtaggable
+   {
+      public Guid EndpointId { get; set; }
+
+      [Sortable(Default = true)]
+      [Searchable]
+      public string Name { get; set; }
+
+      public string Description { get; set; }
+
+      public string Owner { get; set; }
+
+      public string GetEtag()
+      {
+         var serialized = JsonConvert.SerializeObject(this);
+         return Md5Hash.ForString(serialized);
+      }
+   }
+}
---
+++ Multilinks.Core/Models/RootResponse.cs
@@ -0,0 +1,16 @@
+﻿using Multilinks.ApiService.Infrastructure;
+using Newtonsoft.Json;
+
+namespace Multilinks.ApiService.Models
+{
+   public class RootResponse : Resource, IEtaggable
+   {
+      public Link Endpoints { get; set; }
+
+      public string GetEtag()
+      {
+         var serialized = JsonConvert.SerializeObject(this);
+         return Md5Hash.ForString(serialized);
+      }
+   }
+}
